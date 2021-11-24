a3c4482e04d249be2ed81930345692c7aa7cfa2a
Guillaume Cazin <guillaume.cazin@etu.univ-tours.fr>
2021-11-24 19:13:24 +0100

    Feat: Beginning of web page with only head angle control

---
+++ UI.py
@@ -0,0 +1,241 @@
+import asyncio
+import io
+import json
+import math
+import sys
+import flask_helpers
+import cozmo
+import two_hands
+
+try:
+    from flask import Flask, request
+except ImportError:
+    sys.exit("Cannot import from flask: Do `pip3 install --user flask` to install")
+
+try:
+    from PIL import Image, ImageDraw
+except ImportError:
+    sys.exit("Cannot import from PIL: Do `pip3 install --user Pillow` to install")
+
+try:
+    import requests
+except ImportError:
+    sys.exit("Cannot import from requests: Do `pip3 install --user requests` to install")
+
+
+def create_default_image(image_width, image_height, do_gradient=False):
+    '''Create a place-holder PIL image to use until we have a live feed from Cozmo'''
+    image_bytes = bytearray([0x70, 0x70, 0x70]) * image_width * image_height
+
+    if do_gradient:
+        i = 0
+        for y in range(image_height):
+            for x in range(image_width):
+                image_bytes[i] = int(255.0 * (x / image_width))  # R
+                image_bytes[i + 1] = int(255.0 * (y / image_height))  # G
+                image_bytes[i + 2] = 0  # B
+                i += 3
+
+    image = Image.frombytes('RGB', (image_width, image_height), bytes(image_bytes))
+    return image
+
+
+flask_app = Flask(__name__)
+remote_control_cozmo = None
+_default_camera_image = create_default_image(320, 240)
+
+
+class RemoteControlCozmo:
+
+    def __init__(self, coz):
+        self.cozmo = coz
+
+        self.head_up = 0
+        self.head_down = 0
+
+        self.go_fast = 0
+        self.go_slow = 0
+
+        #self.action_queue = []
+
+    def handle_key(self, key_code, is_key_down):
+        '''
+        Called on any key press or release
+        Holding a key down may result in repeated handle_key calls with is_key_down==True
+        '''
+
+        # Update state of head move intent from keyboard, and if anything changed then call update_head
+        update_head = True
+        if key_code == ord('T'):
+            self.head_up = is_key_down
+        elif key_code == ord('G'):
+            self.head_down = is_key_down
+        else:
+            update_head = False
+
+        # Update head as appropriate
+        if update_head:
+            self.update_head()
+
+    def pick_speed(self, fast_speed, mid_speed, slow_speed):
+        if self.go_fast:
+            if not self.go_slow:
+                return fast_speed
+        elif self.go_slow:
+            return slow_speed
+        return mid_speed
+
+    def update_head(self):
+        head_speed = self.pick_speed(2, 1, 0.5)
+        head_vel = (self.head_up - self.head_down) * head_speed
+        self.cozmo.move_head(head_vel)
+
+
+@flask_app.route("/")
+def handle_index_page():
+    return '''
+    <html>
+        <head>
+            <title>User Interface for Cozmo</title>
+        </head>
+        <body>
+            <h1>Finger counter and operation with Cozmo</h1>
+            <table>
+                <tr>
+                    <td valign = top>
+                        <img src="cozmoImage" id="cozmoImageId" width=640 height=480>
+                        <div id="DebugInfoId"></div>
+                    </td>
+                    <td width=30></td>
+                    <td valign=top>
+                        <h2>Controls:</h2>
+
+                        <br>
+                        <b>T</b> : Move Head Up<br>
+                        <b>G</b> : Move Head Down<br>
+
+                    </td>
+                </tr>
+            </table>
+
+            <script type="text/javascript">
+                var gUserAgent = window.navigator.userAgent;
+                var gIsMicrosoftBrowser = gUserAgent.indexOf('MSIE ') > 0 || gUserAgent.indexOf('Trident/') > 0 || gUserAgent.indexOf('Edge/') > 0;
+
+                if (gIsMicrosoftBrowser) {
+                    document.getElementById("cozmoImageMicrosoftWarning").style.display = "block";
+                }
+
+                function postHttpRequest(url, dataSet)
+                {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("POST", url, true);
+                    xhr.send( JSON.stringify( dataSet ) );
+                }
+                
+                function handleKeyActivity (e, actionType)
+                {
+                    var keyCode  = (e.keyCode ? e.keyCode : e.which);
+
+                    postHttpRequest(actionType, {keyCode})
+                }
+
+                document.addEventListener("keydown", function(e) { handleKeyActivity(e, "keydown") } );
+                document.addEventListener("keyup",   function(e) { handleKeyActivity(e, "keyup") } );
+            </script>
+        </body>
+    </html>
+    '''
+
+
+def streaming_video(url_root):
+    '''Video streaming generator function'''
+    try:
+        while True:
+            if remote_control_cozmo:
+                image = remote_control_cozmo.cozmo.world.latest_image
+                image = image.raw_image
+
+                img_io = io.BytesIO()
+                image.save(img_io, 'PNG')
+                img_io.seek(0)
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/png\r\n\r\n' + img_io.getvalue() + b'\r\n')
+            else:
+                asyncio.sleep(.1)
+    except cozmo.exceptions.SDKShutdown:
+        # Tell the main flask thread to shutdown
+        requests.post(url_root + 'shutdown')
+
+
+def serve_single_image():
+    if remote_control_cozmo:
+        try:
+            image = remote_control_cozmo.cozmo.world.latest_image
+            image = image.raw_image
+            if image:
+                return flask_helpers.serve_pil_image(image)
+        except cozmo.exceptions.SDKShutdown:
+            requests.post('shutdown')
+    return flask_helpers.serve_pil_image(_default_camera_image)
+
+
+def is_microsoft_browser(request):
+    agent = request.user_agent.string
+    return 'Edge/' in agent or 'MSIE ' in agent or 'Trident/' in agent
+
+
+@flask_app.route("/cozmoImage")
+def handle_cozmoImage():
+    if is_microsoft_browser(request):
+        return serve_single_image()
+    return flask_helpers.stream_video(streaming_video, request.url_root)
+
+
+def handle_key_event(key_request, is_key_down):
+    message = json.loads(key_request.data.decode("utf-8"))
+    if remote_control_cozmo:
+        remote_control_cozmo.handle_key(key_code=(message['keyCode']), is_key_down=is_key_down)
+    return ""
+
+
+@flask_app.route('/shutdown', methods=['POST'])
+def shutdown():
+    flask_helpers.shutdown_flask(request)
+    return ""
+
+
+@flask_app.route('/keydown', methods=['POST'])
+def handle_keydown():
+    '''Called from Javascript whenever a key is down (note: can generate repeat calls if held down)'''
+    return handle_key_event(request, is_key_down=True)
+
+
+@flask_app.route('/keyup', methods=['POST'])
+def handle_keyup():
+    '''Called from Javascript whenever a key is released'''
+    return handle_key_event(request, is_key_down=False)
+
+
+def run(sdk_conn):
+    robot = sdk_conn.wait_for_robot()
+    robot.enable_device_imu(True, True, True)
+
+    global remote_control_cozmo
+    remote_control_cozmo = RemoteControlCozmo(robot)
+
+    # Turn on image receiving by the camera
+    robot.camera.image_stream_enabled = True
+
+    flask_helpers.run_flask(flask_app)
+
+
+if __name__ == '__main__':
+    cozmo.setup_basic_logging()
+    cozmo.robot.Robot.drive_off_charger_on_connect = False  # RC can drive off charger if required
+    try:
+        cozmo.connect(run)
+    except KeyboardInterrupt as e:
+        pass
+    except cozmo.ConnectionError as e:
+        sys.exit("A connection error occurred: %s" % e)
